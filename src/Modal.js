be8e97e5fcf2cf04ca795fdfcc4804fb889233a4
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-15 13:12:22 +0430

    init

--- src/Modal.js
+++ src/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 const Modal = () => {
-  return <div className={`modal-overlay show-modal`}>
+  return <div className={`modal-overlay`}>
     <div className="modal-container">
       <h3>modal content</h3>
       <button className='close-modal-btn'>
