1c3cd70846c2736e5179a910c0cc20ec4b34ace4
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-15 13:01:12 +0430

    init

--- src/Modal.js
+++ src/Modal.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 const Modal = () => {
-  return <div className={`modal-overlay`}></div>
+  return <div className={`modal-overlay show-modal`}>
+    <div className="modal-container">
+      <h3></h3>
+    </div>
+  </div>
+
 }
 
 export default Modal
