77bc19b6054c2912db6469b897c72b30e4add641
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-15 09:41:47 +0430

    init

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,7 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 import { useGlobalContext } from "./context";
  
-const Sidebar = () => {
+const Sidebar = () => { 
   const { isSidebarOpen, closeSidebar } = useGlobalContext()
 
   return <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
