e44a57c9879e8f2313aadb77c014d521869578ac
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-15 12:04:56 +0430

    init

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,10 @@ import { FaBars } from 'react-icons/fa'
 const Home = () => {
   return <main>
     <button className='sidebar-toggle'>
-
+      <FaBars />
     </button>
+    <button className='btn'>show modal</button>
+    
   </main>
 }
 
